c59beef30cc3ba42fed389cc07d54749213e1685
morali777 <68974871+morali777@users.noreply.github.com>
2020-11-12 09:39:04 -0500

    initial commit

---
+++ src/main/java/SweetNovember.java
@@ -0,0 +1,5 @@
+public class SweetNovember {
+    public static void main(String[] args) {
+        System.out.println("November");
+    }
+}
---
+++ src/test/java/novemberNovember.java
@@ -0,0 +1,5 @@
+public class novemberNovember {
+    public static void main(String[] args) {
+        System.out.println("sweet");
+    }
+}
